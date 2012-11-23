a48b7dfcb92cc31b4e35ca18d19bacb00723aa9d
macournoyer <macournoyer@gmail.com>
2012-11-23 14:50:46 -0500

    Refactor and add tests.

--- sandbox/shunting_yard.rb
+++ sandbox/shunting_yard.rb
@@ -60,11 +60,14 @@ class Message
     @name
   end
 
+  # Detatch the next message and returns it
   def detatch
+    m = @next
     @next = nil
-    self
+    m
   end
 
+  # Get the tail of this message chain
   def tail
     if @next
       @next.tail
@@ -73,21 +76,90 @@ class Message
     end
   end
 
+  def last?
+    @next.nil?
+  end
+
+  # Detach the last message from the chain
   def pop
-    if @next.nil?
+    if last?
       nil
-    elsif @next.next.nil?
-      n = @next
-      @next = nil
-      n
+    elsif @next.last?
+      detatch
     else
       @next.pop
     end
   end
 
+  # Append the message as the tail of the chain
   def append(m)
     tail.next = m
     self
+  end
+
+  def shuffle
+    #### Shunting yard algo ####
+    output_queue = []
+    stack = []
+    m = self
+
+    while m
+      if m.operator
+        m2 = stack.last
+        while m2 && ((m.operator.left_to_right? && m.operator.precedence <= m2.operator.precedence) ||
+                     (m.operator.right_to_left? && m.operator.precedence < m2.operator.precedence))
+          output_queue.push stack.pop
+          m2 = stack.last
+        end
+        stack.push m
+      else
+        output_queue.push m
+        # Advance to the next operator
+        # TODO perhaps could refactor to combine w/ parent while?
+        m = m.next until m.next.nil? || m.next.operator
+      end
+
+      # Cut it from the message chain
+      m = m.detatch
+    end
+
+    while operator = stack.pop
+      output_queue.push operator
+    end
+
+    # Convert from RPN
+    stack = []
+    while m = output_queue.shift
+      if m.operator
+        case true
+        when m.operator.unary?
+          m.args = [stack.pop]
+          stack.push m
+        when m.operator.binary?
+          m.args = [stack.pop]
+          m2 = stack.pop
+          m2.append m
+          stack.push m2
+        when m.operator.ternary?
+          val = stack.pop
+          receiver = stack.pop
+          if name = receiver.pop
+            m.args = [name, val]
+            receiver.append m
+            stack.push receiver
+          else
+            m.args = [receiver, val]
+            stack.push m
+          end
+        end
+      else
+        stack.push m
+      end
+    end
+
+    raise "Parsing error: things left on the stack" unless stack.size == 1
+
+    stack.first
   end
 end
 
@@ -95,83 +167,21 @@ def M(*args)
   Message.new(*args)
 end
 
-# m = M("1", M("+", M("2")))
-# m = M("1", M("+", M("2", M("*", M("3")))))
-# m = M("1", M("*", M("2", M("+", M("3")))))
-# m = M("x", M("=", M("2", M("+", M("3")))))
-# m = M("x", M("=", M("!", M("2", M("+", M("3", M("*", M("1"))))))))
-m = M("object", M("prop", M("=", M("x", M("y")))))
-# m = M("object", M("=", M("x", M("y", M("x")))))
-# m = M("x", M("a", M("=", M("e", M("d")))))
-# m = M("a", M("b", M("+", M("c", M("d")))))
-puts "input: " + m.fullname
-
-
-#### Shunting yard algo ####
-output_queue = []
-stack = []
-
-while m
-  if m.operator
-    m2 = stack.last
-    while m2 && ((m.operator.left_to_right? && m.operator.precedence <= m2.operator.precedence) ||
-                 (m.operator.right_to_left? && m.operator.precedence < m2.operator.precedence))
-      output_queue.push stack.pop
-      m2 = stack.last
+if __FILE__ == $PROGRAM_NAME
+  require "test/unit"
+  
+  class ShufflingTest < Test::Unit::TestCase
+    def test_case_name
+      assert_equal "1 +(2)", M("1", M("+", M("2"))).shuffle.fullname
+      assert_equal "1 +(2 *(3))", M("1", M("+", M("2", M("*", M("3"))))).shuffle.fullname
+      assert_equal "1 *(2) +(3)", M("1", M("*", M("2", M("+", M("3"))))).shuffle.fullname
+      assert_equal "=(x, 2 +(3))", M("x", M("=", M("2", M("+", M("3"))))).shuffle.fullname
+      assert_equal "=(x, !(2) +(3 *(1)))", M("x", M("=", M("!", M("2", M("+", M("3", M("*", M("1")))))))).shuffle.fullname
+      assert_equal "object =(prop, x y)", M("object", M("prop", M("=", M("x", M("y"))))).shuffle.fullname
+      assert_equal "=(object, x y x)", M("object", M("=", M("x", M("y", M("x"))))).shuffle.fullname
+      assert_equal "x =(a, e d)", M("x", M("a", M("=", M("e", M("d"))))).shuffle.fullname
+      assert_equal "a b +(c d)", M("a", M("b", M("+", M("c", M("d"))))).shuffle.fullname
+      assert_equal "=(a, =(b, c))", M("a", M("=", M("b", M("=", M("c"))))).shuffle.fullname
     end
-    stack.push m
-  else
-    output_queue.push m
-    # Advance to the next operator
-    # TODO perhaps could refactor to combine w/ parent while?
-    m = m.next until m.next.nil? || m.next.operator
-  end
-
-  # Cut it from the message chain
-  next_m = m.next
-  m.next = nil
-  m = next_m
-end
-
-while operator = stack.pop
-  output_queue.push operator
-end
-
-print "output_queue: "
-p output_queue.map(&:fullname)
-
-
-# Convert from RPN
-stack = []
-while m = output_queue.shift
-  if m.operator
-    case true
-    when m.operator.unary?
-      m.args = [stack.pop]
-      stack.push m
-    when m.operator.binary?
-      m.args = [stack.pop]
-      m2 = stack.pop
-      m2.append m
-      stack.push m2
-    when m.operator.ternary?
-      val = stack.pop
-      receiver = stack.pop
-      if name = receiver.pop
-        m.args = [name, val]
-        receiver.append m
-        stack.push receiver
-      else
-        m.args = [receiver, val]
-        stack.push m
-      end
-    end
-  else
-    stack.push m
   end
 end
-
-raise "Parsing error: things left on the stack" unless stack.size == 1
-
-
-puts "output: " + stack.first.fullname
