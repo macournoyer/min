5fbef90040837158696d57619f04f51476373e1d
Laurent Laborde <kerdezixe@gmail.com>
2014-04-15 15:44:49 +0200

    Adding useless comments

--- src/min/Min.java
+++ src/min/Min.java
@@ -2,17 +2,25 @@ package min;
 
 import min.lang.MinObject;
 import min.lang.Message;
-//import min.lang.Method;
 import min.lang.File;
 import min.lang.Bootstrap;
-//import java.util.ArrayList;
 
+/*  Simplest of the simple stuff
+    Entry point for main :
+        Parsing args and setting flags
+        Running the bootstrap
+        Parse & eval code
+ */
 public class Min {
+
+    /* main ! duh ! */
     public static void main(String[] args) throws Exception {
+
         String code = null;
         String file = "<eval>";
         boolean debug = false;
 
+        /* parsing args */
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-e")) code = args[++i];
             else if (args[i].equals("-d")) debug = true;
@@ -24,13 +32,15 @@ public class Min {
             usage();
         }
 
+        /* Run bootstrap, parse & eval code */
         new Bootstrap().run();
         Message message = Message.parse(code, file);
         if (debug) System.out.println(message.fullName());
         message.evalOn(MinObject.lobby);
+
     }
 
-    // Print help and exit
+    /* Print help and exit */
     public static void usage() {
         System.out.println("usage: min [-d] < -e code | file.min >");
         System.exit(1);
