4777ffa146c54b2bba995f4b35d0715cf9be937d
Laurent Laborde <kerdezixe@gmail.com>
2014-04-30 14:58:47 +0200

    REPL in progress :)

--- src/min/Min.java
+++ src/min/Min.java
@@ -5,6 +5,8 @@ import min.lang.Message;
 import min.lang.File;
 import min.lang.Bootstrap;
 
+import java.util.Scanner;
+
 /*  Simplest of the simple stuff
     Entry point for main :
         Parsing args and setting flags
@@ -26,6 +28,7 @@ public class Min {
             else if (args[i].equals("-d")) debug = true;
             else if (args[i].equals("-h")) usage();
             else if (args[i].equals("--help")) usage();
+            else if (args[i].equals("-x")) repl();
             else code = File.read(file = args[i]);
         }
 
@@ -46,6 +49,26 @@ public class Min {
     /* Print help and exit */
     private static void usage() {
         System.out.println("usage: min [-d] < -e code | file.min >");
+        System.out.println("       -x REPL (not implemented yet)");
+        System.exit(1);
+    }
+
+    private static void repl() {
+        Boolean LoopAgain = true;
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("REPL not implemented yet");
+        System.out.println("------------------------");
+        System.out.println("Type 'bye' to exit\n");
+
+        while(LoopAgain) {
+            System.out.print("min> ");
+            input = scanner.nextLine();
+            System.out.println(input);
+            if(input.equals("bye")) LoopAgain = false;
+        }
+        System.out.println("Bye!\n");
         System.exit(1);
     }
 
